1e61d2d87e57c1f8d2902ee72f78e1cb761dc413
matty199905 <matty199905@gmail.com>
2025-08-22 16:18:05 -0300

    final-2.0

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -12,7 +12,7 @@ const Layout = ({ children }) => {
   color === 'dark' &&
   <Aurora
     colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
-    blend={5}
+    blend={0.5}
     amplitude={1.0}
     speed={0.8}
   />
